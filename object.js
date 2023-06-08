d43758c1c7f5a3cd1eb600c90dede5bdc513ab21
alvintri <102010507+alvintri@users.noreply.github.com>
2023-06-08 11:11:38 +0700

    Update object.js

--- object.js
+++ object.js
@@ -3,7 +3,7 @@ var dataKendaraan = ["Ujang",24,"Avanza","B 1234 AGG",600000,false]
 var dataSTNKUjang = {
     "nama"  : "Ujang",
     "usia" : 24,
-    "tipe" :"Avanza",
+    "tipe" :"Yaris",
     "plat" : "B 1234 AGG",
     "biaya" : 600000,
     "isBayar" : false,
